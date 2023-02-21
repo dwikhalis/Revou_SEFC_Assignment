b71e4b74860b32063ed4718453d99c26d700bf07
khalis dwi <dwikhalis@gmail.com>
2023-02-21 16:51:49 +0700

    index.html message form constraint alerts

--- JS/index.js
+++ JS/index.js
@@ -22,21 +22,54 @@ function previewMessage() {
     let userName = document.getElementById('user-name').value
     let birthDate = document.getElementById('birth-date').value
     let userMessage = document.getElementById('user-message').value
+    let userGender = document.getElementsByName('gender')
+    let nullForm = []
     let date = new Date()
 
-    let userGender = document.getElementsByName('gender')
-
-    for (let i = 0; i < userGender.length; i++) {
-        if (userGender[i].checked) {
-            userGender = userGender[i].value
-        }
+    if (userName === "") {
+        nullForm.push(" Name")
+        document.getElementById('user-name').style.border = '3px solid red'
+    } else {
+        document.getElementById('user-name').style.border = ''
+    }
+    
+    if (birthDate === "") {
+        nullForm.push(" Birth Date")
+        document.getElementById('birth-date').style.border = '3px solid red'
+    } else {
+        document.getElementById('birth-date').style.border = ''
+    }
+    
+    if (userMessage === "") {
+        nullForm.push(" Message")
+        document.getElementById('user-message').style.border = '3px solid red'
+    } else {
+        document.getElementById('user-message').style.border = ''
     }
 
-    document.getElementById('preview-box').innerHTML =
+    if (nullForm.length > 0) {
+        document.getElementById('preview-box').innerHTML = ""
+
+        document.getElementById('constraint-alert').innerHTML =
+        "Please insert your" + nullForm
+        
+        console.log(nullForm)
+
+    } else {       
+        document.getElementById('constraint-alert').innerHTML = ""
+        
+        for (let i = 0; i < userGender.length; i++) {
+            if (userGender[i].checked) {
+                userGender = userGender[i].value
+            }
+        }
+        
+        document.getElementById('preview-box').innerHTML =
         "Submit Time : " + date + '<br>' +
         "Name : " + userName + '<br>' +
         "Birth Date : " + birthDate + '<br>' +
         "Gender : " + userGender + '<br>' + '<br>' +
         "Message : " + '<br>' +
         userMessage
+    }
 }
