67720b03a8ea5bbc93f385183e91c746f79e2db9
khalis dwi <dwikhalis@gmail.com>
2023-02-23 09:13:53 +0700

    feat: banner next and prev

--- JS/index.js
+++ JS/index.js
@@ -1,22 +1,59 @@
+//! BANNER SLIDE
+let slideNum = 5
+let slideCount = 0
+
+document.getElementById(`banner${slideCount}`).style.display = 'block'
+
+document.getElementById("next").addEventListener("click", nextSlide)
+document.getElementById("prev").addEventListener("click", prevSlide)
+
+function nextSlide() {
+    slideCount++
+
+    if (slideCount === slideNum) {
+        document.getElementById(`banner${slideCount - 1}`).style.display = 'none'
+        slideCount = 0
+        document.getElementById(`banner${slideCount}`).style.display = 'block'
+    } else {
+        document.getElementById(`banner${slideCount}`).style.display = 'block'
+        document.getElementById(`banner${slideCount - 1}`).style.display = 'none'
+    }
+    console.log(slideCount)
+}
+
+function prevSlide() {
+    slideCount--
+
+    if (slideCount === -1) {
+        document.getElementById(`banner${slideCount + 1}`).style.display = 'none'
+        slideCount = 4
+        document.getElementById(`banner${slideCount}`).style.display = 'block'
+    } else {
+        document.getElementById(`banner${slideCount}`).style.display = 'block'
+        document.getElementById(`banner${slideCount + 1}`).style.display = 'none'
+    }
+    console.log(slideCount)
+}
+
 //! BURGER DROPDOWN
 document.getElementById("burger-button").addEventListener("click", burgerDrop)
 
 let drop = true
 
-function burgerDrop (event) {
-    
+function burgerDrop(event) {
+
     if (event.isTrusted === true && drop === true) {
         document.getElementById('burger-dropdown').style.display = 'flex'
         drop = false
     } else {
         document.getElementById('burger-dropdown').style.display = 'none'
-        drop = true 
+        drop = true
     }
-    
+
     console.log(drop)
 }
 
-document.getElementById("submit").addEventListener("click", previewMessage)
+//! POP UP INPUT NAME 
 document.getElementById("popup-close").addEventListener("click", closePopup)
 document.addEventListener("keypress", nameEnter)
 
@@ -36,12 +73,16 @@ function closePopup(event) {
     event.preventDefault()
 }
 
-function previewMessage() {
+//! FORM VALIDATION
+document.getElementById("submit").addEventListener("click", validate)
+
+function validate() {
+
     let userName = document.getElementById('user-name').value
     let birthDate = document.getElementById('birth-date').value
     let userMessage = document.getElementById('user-message').value
-    let userGender = document.getElementsByName('gender')
     let nullForm = []
+    let userGender = document.getElementsByName('gender')
     let date = new Date()
 
     if (userName === "") {
@@ -50,14 +91,14 @@ function previewMessage() {
     } else {
         document.getElementById('user-name').style.border = ''
     }
-    
+
     if (birthDate === "") {
         nullForm.push(" Birth Date")
         document.getElementById('birth-date').style.border = '3px solid red'
     } else {
         document.getElementById('birth-date').style.border = ''
     }
-    
+
     if (userMessage === "") {
         nullForm.push(" Message")
         document.getElementById('user-message').style.border = '3px solid red'
@@ -69,25 +110,28 @@ function previewMessage() {
         document.getElementById('preview-box').innerHTML = ""
 
         document.getElementById('constraint-alert').innerHTML =
-        "Please insert your" + nullForm
-        
+            "Please insert your" + nullForm
+
         console.log(nullForm)
 
-    } else {       
+    } else {
+
+        //! PREVIEW MESSAGE
         document.getElementById('constraint-alert').innerHTML = ""
-        
+
         for (let i = 0; i < userGender.length; i++) {
             if (userGender[i].checked) {
                 userGender = userGender[i].value
             }
         }
-        
+
         document.getElementById('preview-box').innerHTML =
-        "Submit Time : " + date + '<br>' +
-        "Name : " + userName + '<br>' +
-        "Birth Date : " + birthDate + '<br>' +
-        "Gender : " + userGender + '<br>' + '<br>' +
-        "Message : " + '<br>' +
-        userMessage
+            "Submit Time : " + date + '<br>' +
+            "Name : " + userName + '<br>' +
+            "Birth Date : " + birthDate + '<br>' +
+            "Gender : " + userGender + '<br>' + '<br>' +
+            "Message : " + '<br>' +
+            userMessage
     }
+
 }
